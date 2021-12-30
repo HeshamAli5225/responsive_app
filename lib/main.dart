2dcdfcc64b503beb1af1060e99ca45810310fd95
HeshamAli <hesham.ali.3959@gmail.com>
2021-12-31 00:13:10 +0200

    commit message => the code of the second branch

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // first code in branch
-  //code in Second_branch
+  //code in the Second_branch
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
